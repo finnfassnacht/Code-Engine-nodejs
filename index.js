b0d9e23e71c3639f016f2cd6e820d7f0e5539584
finn <finnfassnacht@gmail.com>
2023-04-18 10:12:06 +0200

    test

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ PORT = 8080
 app.use(express.json())
 const router = express.Router()
 app.use("/api", router)
-router.get("/hello", (req,res) => {
+router.get("/", (req,res) => {
 	res.json({"msg":"hello world!!!"})
 })
 
-router.get("/test", (req,res) => {
-	res.json({"msg":"this is a test."})
-})
 
 
 
