fe4e3f2ab70fbeddef3b35aa1ecc58d743f69cdc
finn <finnfassnacht@gmail.com>
2023-04-18 10:14:38 +0200

    add comments

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+// require expressjs
 const express = require("express")
 const app = express()
 PORT = 8080
 app.use(express.json())
+// use router to bundle all routes to /api
 const router = express.Router()
 app.use("/api", router)
+// get on root route (/api)
 router.get("/", (req,res) => {
 	res.json({"msg":"hello world!!!"})
 })
-
-
-
-
+// start server
 app.listen(PORT, () => {
 	console.log("Server is up and running!!")
 })
