9921a84b6eb511242b43c85f76602bedd6f3814e
Finn-jf <finnfassnacht@gmail.com>
2023-04-20 10:37:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // require expressjs
 const express = require("express")
 const app = express()
+// define port 8080
 PORT = 8080
 app.use(express.json())
-// use router to bundle all routes to /api
+// use router to bundle all routes to /
 const router = express.Router()
 app.use("/", router)
-// get on root route (/api)
+// get on root route 
 router.get("/", (req,res) => {
-	res.json({"msg":"hello wooooorld!!!"})
+	res.send("hello world!")
 })
 // start server
 app.listen(PORT, () => {
